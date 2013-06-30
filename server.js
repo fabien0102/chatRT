a15c2a239eeefde357a429576f6e4fd7e4c8baa0
fabien0102 <fabien0102@planee.fr>
2013-06-30 19:55:20 +0200

    Add simple client access

--- server.js
+++ server.js
@@ -1,5 +1,14 @@
-var io = require('socket.io').listen(1337);
+var express = require('express'),
+    app = express(),
+    server = require('http').createServer(app),
+    io = require('socket.io').listen(server);
+server.listen(1337);
 var md5 = require('md5');
+
+app.use(express.static(__dirname + '/public'));
+app.get('/', function (req, res) {
+    res.sendfile(__dirname + '/public/index.html');
+});
 
 var users = {};
 var messages = [];
